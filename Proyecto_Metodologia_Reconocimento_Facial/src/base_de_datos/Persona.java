bfa8b5dfce1dd1dcebd432fc89cd8649e11b7de5
nestor <nramirez17@alumnos.utalca.cl>
2020-12-27 23:27:21 -0300

    codificación clase persona

--- Proyecto_Metodologia_Reconocimento_Facial/src/base_de_datos/Persona.java
+++ Proyecto_Metodologia_Reconocimento_Facial/src/base_de_datos/Persona.java
@@ -3,12 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto_metodologia_reconocimento_facial;
+package base_de_datos;
 
 /**
  *
  * @author sebas
  */
 public class Persona {
+    public String nombre = null;
+    public String infoAdicional = null;
     
+    public Persona (String nombre, String infoAdicional){
+        this.nombre = nombre;
+        this.infoAdicional = infoAdicional;
+    }
 }
